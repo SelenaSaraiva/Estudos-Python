9c9fdb1eae022b01cbc095404fc882d79299a13a
SelenaSaraiva <89755025+SelenaSaraiva@users.noreply.github.com>
2023-08-23 20:52:57 -0300

    Novas infos

---
+++ Curso_Python_Udemy/CaracterEscape.py
@@ -0,0 +1,7 @@
+print('Selena \"Saraiva\"')
+# Caractere de escape, muito utilizado caso seja necessário adicionar aspas dentro do texto, por exemplo. Sem a barra invertida, o interpretador 
+# leria as aspas adicionais. 
+
+print('Selena "Saraiva"')
+# Uma outra forma de realizarmos esse processo, é 'inverter' as aspas. Colocarmos aspas duplas como adicionais e aspas simples como principais. 
+#(Aspas duplas dentro de aspas simples) - OBS: o inverso também funciona (aspas simples dentro de aspas duplas)
---
+++ Curso_Python_Udemy/SepEnd.py
@@ -0,0 +1,5 @@
+# As informações dentro do print, são ARGUMENTOS não nomeados (não levam funcionalidade)
+# Sep=; end=; são argumentos nomeados (levam uma funcionalidade)
+
+print(12, 34, sep='-', end='\n##')
+print(56, 78, sep='-')
