611eadda36fca8cdb35034cde8d8375d03c2a417
SelenaSaraiva <89755025+SelenaSaraiva@users.noreply.github.com>
2023-09-01 15:00:56 -0300

    Variáveis

---
+++ Curso_Python_Udemy/Variaveis.py
@@ -0,0 +1,13 @@
+nome = 'Selena'
+sobrenome = 'Saraiva'
+idade = 21
+AnoNasc = 2023 - idade
+MaiorIdade = idade >= 18
+AlturaMetros = 1.60
+
+print('Nome: ', nome)
+print('Sobrenome: ',sobrenome )
+print('Idade: ', idade)
+print('Ano de nascimento: ', AnoNasc)
+print('Maior de idade? ', MaiorIdade)
+print('Altura em metros: ', AlturaMetros)
