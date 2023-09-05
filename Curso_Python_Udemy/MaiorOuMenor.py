0d164b7f7b632470267aacd4f2c0ff4b48318a40
SelenaSaraiva <89755025+SelenaSaraiva@users.noreply.github.com>
2023-09-05 15:06:45 -0300

    Maior ou menor valor  - simples teste

---
+++ Curso_Python_Udemy/MaiorOuMenor.py
@@ -0,0 +1,9 @@
+n1 = int(input('Informe o 1° número: '))
+n2 = int(input('Informe o 2° número: '))
+
+if n1 > n2: 
+    print(f'O 1° valor = {n1}, é maior que o 2° valor = {n2}')
+else: 
+    print(f'O 2° valor = {n2}, é maior que o 1° valor = {n1}')
+
+print('Análise finalizada!')
